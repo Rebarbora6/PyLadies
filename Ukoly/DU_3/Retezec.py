87c715e9cc92ba7b1a4cae4f859df8822dbd268e
Anna Kašíková <anna.kasikova6@gmail.com>
2022-10-12 19:17:42 +0200

    Oprava programu Retezec.py

--- Ukoly/DU_3/Retezec.py
+++ Ukoly/DU_3/Retezec.py
@@ -1,12 +1,16 @@
-user_input = str(input('Napiš string, který chceš změnit: '))
-sequence = int(input('Kolikáté písmeno se má vyměnit? '))
-while (len(user_input) < sequence):
-    print('Je potřeba zadat číslo, které je menší nebo rovno počtu znaků.')
+user_input = (input('Napiš string, který chceš změnit: '))
+sequence = (input('Kolikáté písmeno se má vyměnit? '))
+while sequence.isnumeric() == False:
+    print('Je potřeba zadat kladné číslo, ne písmeno.')
+    sequence = (input('Kolikáté písmeno se má vyměnit? '))
+
+while (len(user_input) < int(sequence)) or (0 >= int(sequence)):
+    print('Je potřeba zadat kladné číslo, které je menší nebo rovno počtu znaků.')
     sequence = int(input('Kolikáté písmeno se má vyměnit? '))
 
-character = str(input('Napiš nové písmeno ve stringu: '))
+character = (input('Napiš nové písmeno ve stringu: '))
 
-new_string = '{}{}{}'.format(user_input[:(sequence-1)],
-character, user_input[sequence:])
+new_string = '{}{}{}'.format(user_input[:(int(sequence)-1)],
+character, user_input[int(sequence):])
 
-print('Nový string zní: ', new_string)
+print('Nový string zní:', new_string)
