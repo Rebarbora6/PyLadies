312441b1df1c15f1b0d8ffb79eed1e95d5aea8cd
Anna Kašíková <anna.kasikova6@gmail.com>
2022-11-01 21:59:22 +0100

    First corrections

--- Ukoly/DU_6/Piskvorky.py
+++ Ukoly/DU_6/Piskvorky.py
@@ -1,62 +1,63 @@
 from random import randrange
 
-WINNER = None
+winner = None
 
 
 def evaluation(string):
-    for _ in string:
-        if 'xxx' in string and 'ooo' not in string:
-            WINNER = '"x"'
-        elif 'ooo' in string and 'xxx' not in string:
-            WINNER = '"o"'
-        elif '-' in string and ('xxx' not in string or 'ooo' not in string):
-            WINNER = None
-        elif '-' not in string and \
-            ('xxx' not in string or 'ooo' not in string):
-            WINNER = 'no one'
-    if WINNER is not None:
-        print('The winner is ' + WINNER)
-    return(WINNER)
+    if 'xxx' in string and 'ooo' not in string:
+        winner = '"x"'
+    elif 'ooo' in string and 'xxx' not in string:
+        winner = '"o"'
+    elif '-' in string and ('xxx' not in string or 'ooo' not in string):
+        winner = None
+    elif '-' not in string and \
+        ('xxx' not in string or 'ooo' not in string):
+        winner = 'no one'
+    if winner is not None:
+        print('The winner is ' + winner)
+    return winner
+
+
+def format_of_field(field_input, position, sign):
+    start = field_input[:position]
+    end = field_input[position + 1:]
+    field_output = '{}{}{}'.format(start, sign, end)
+    print(field_output)
+    return field_output
 
 
 def move_of_player(field, sign):
     number_of_field = input('Pick your position. ')
+    while not number_of_field.isnumeric():
+        print('Use only numbers in range. ')
+        number_of_field = input('Pick your position. ')
+
     while True:
-        number_of_field = str(int(number_of_field) - 1)
-        if not number_of_field.isnumeric():
-            print('Use only numbers in range. ')
-            number_of_field = input('Pick your position. ')
-        elif int(number_of_field) >= 20 or int(number_of_field) < 0:
+        number_of_field = int(number_of_field) - 1
+
+        if number_of_field >= 20 or number_of_field < 0:
             print('The number is too big or too small. ')
             number_of_field = input('Pick your position. ')
-        elif field[int(number_of_field)] == 'x' or \
-            field[int(number_of_field)] == 'o':
+        elif field[number_of_field] == 'x' or \
+            field[number_of_field] == 'o':
             print('This position is already used. ')
             number_of_field = input('Use another position. ')
         else:
             break
-    number_of_field = int(number_of_field)
-    start = field[:number_of_field]
-    end = field[number_of_field + 1:]
-    field_player = '{}{}{}'.format(start, sign, end)
-    print(field_player)
+
+    field_player = format_of_field(field, number_of_field, sign)
     return field_player
 
 
 def move_of_computer(field, sign):
     while True:
-        if evaluation == 'no one':
-            break
         number_of_field = randrange(0, 19)
         if field[number_of_field] == 'x' or field[number_of_field] == 'o':
             number_of_field = randrange(0, 19)
         else:
             break
 
-    start = field[:number_of_field]
-    end = field[number_of_field + 1:]
-    field_computer = '{}{}{}'.format(start, sign, end)
-    print(field_computer)
+    field_computer = format_of_field(field, number_of_field, sign)
     return field_computer
 
 
@@ -67,8 +68,9 @@ def piskvorky1d():
     field_computer = field
     while evaluation(field_player) is None:
         field_player = move_of_computer(field_computer, 'x')
-        if evaluation(field_computer) is not None:
-            return
         field_computer = move_of_player(field_player, 'o')
+        if evaluation(field_computer) is not None or \
+        evaluation(field_computer) == 'no one':
+            return
 
 piskvorky1d()
--- Ukoly/DU_6/Rodne_cislo_f.py
+++ Ukoly/DU_6/Rodne_cislo_f.py
@@ -1,32 +1,34 @@
 MONTH_30 = [4, 6, 9, 11]
 MONTH_31 = [1, 3, 5, 7, 8, 10, 12]
 
-number = input('Enter the birth number: ')
-while len(number) != 11:
-    print('The number has to be 11 characters long')
+
+def main():
     number = input('Enter the birth number: ')
+    while len(number) != 11:
+        print('The number has to be 11 characters long')
+        number = input('Enter the birth number: ')
+
+    return number
 
 
 def format(birth_number):
-    format = birth_number.split('/')
-    without_slash = ''.join(format)
-    if not (without_slash.isnumeric):
+    separate = birth_number.split('/')
+    without_slash = ''.join(separate)
+    if not without_slash.isnumeric():
         print('You can use only digits or slash "/"')
         return False
     if birth_number.count('/') > 1:
         print('There should be only one separator')
         return False
-    else:
-        if birth_number[6] != '/':
-            print('Separator has to be slash "/" in 7th position')
-            return False
-        else:
-            return True
+    if birth_number[6] != '/':
+        print('Separator has to be slash "/" in 7th position')
+        return False
+    return True
 
 
 def divisibility(birth_number):
-    format = birth_number.split('/')
-    without_slash = ''.join(format)
+    separate = birth_number.split('/')
+    without_slash = ''.join(separate)
     if int(without_slash) % 11 != 0:
         print('The number hast to be divisible by 11')
         return False
@@ -37,24 +39,22 @@ def divisibility(birth_number):
 def sex(month):
         if month > 50:
             month = month - 50
-            print('Sex: Female')
             return 'Female'
-        elif month > 0 and month <= 12:
-            print('Sex: Male')
+        if month > 0 and month <= 12:
             return 'Male'
-        else:
+        if month <= 0 or (month > 12 and month <= 50) or month > 62:
             print('The value of month of birth is incorrect')
-            return False
+            return None
 
 
 def date_of_birth(day, month, year):
         if month > 50:
             month = month - 50
         if int(year) >= 85:
-            year = int('19' + number[0:2])
+            year = int('19' + str(year))
         elif int(year) <= 22 and \
         int(year) >= 0:
-            year = int('20' + number[0:2])
+            year = int('20' + str(year))
         else:
             print('The birth number was publicated before 1985')
             return False
@@ -74,13 +74,13 @@ def date_of_birth(day, month, year):
             return False
 
 
-def analyze(birth_number):
-        if format(birth_number) is True:
-            if divisibility(birth_number) is True:
-                DAY = int(birth_number[4:6])
-                MONTH = int(birth_number[2:4])
-                YEAR = int(birth_number[0:2])
-                date_of_birth(DAY, MONTH, YEAR)
-                sex(MONTH)
+def analyze():
+    birth_number = main()
+    if format(birth_number) == True and divisibility(birth_number) == True:
+        day = int(birth_number[4:6])
+        month = int(birth_number[2:4])
+        year = int(birth_number[0:2])
+        date_of_birth(day, month, year)
+        print('Sex:', sex(month))
 
-analyze(number)
+analyze()
