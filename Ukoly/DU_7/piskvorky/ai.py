5c1c9db99b25e3e0bb0905a055b183ffe980811e
Anna Kašíková <anna.kasikova6@gmail.com>
2022-11-14 20:50:47 +0100

    Commit of task No. 0 and 1 - Piskvorky.py and tests

---
+++ Ukoly/DU_7/piskvorky/ai.py
@@ -0,0 +1,19 @@
+from random import randrange
+
+
+def move_of_computer(field, sign):
+    while True:
+        number_of_field = randrange(0, 19)
+        if field[number_of_field] == 'x' or field[number_of_field] == 'o':
+            number_of_field = randrange(0, 19)
+        else:
+            break
+    field_computer = format_of_field(field, number_of_field, sign)
+    return field_computer
+
+def format_of_field(field_input, position, sign):
+    start = field_input[:position]
+    end = field_input[position + 1:]
+    field_output = '{}{}{}'.format(start, sign, end)
+    print(field_output)
+    return field_output
